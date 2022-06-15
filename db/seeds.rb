123dd2f6ccfb7db8906f3f46f2893f5bcfd4d9ed
joskalenda <joskalenda3@gmail.com>
2022-06-15 22:56:56 +0200

    Add routes for Api app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(
+  [
+    { content: 'Hello Hello, world!' },
+    { content: 'Goodbye Goodbye, world!' },
+    { content: 'Good morning Good morning, world!' },
+    { content: 'Good night Good night, world!' },
+    { content: 'Good evening Good evening, world!' }
+  ])
